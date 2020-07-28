ca6c6b950e88cbacd695b7a177621404828daae8
manishmi <manishmi@MANISHMI-IN.in.oracle.com>
2020-07-28 20:19:37 +0530

    Randomly generate an array of bombs and valid boxes

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
 	const grid = document.querySelector('.grid')
 
 	let width = 10
+	let bombAmount = 20
 	let squares = []
 
 	//create board
 	function createBoard() {
-		let i = 0;
-		while (i < width * width) {
+		// get shuffeld game array with random bombs
+		const bombArray = Array(bombAmount).fill('bomb')
+		const emptyArray = Array(width * width - bombAmount).fill('valid')
+		const gameArray = emptyArray.concat(bombArray)
+		const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
+
+		for (let i = 0; i < width * width; i++) {
 			const square = document.createElement('div')
 			square.setAttribute('id', i)
 			grid.appendChild(square)
 			squares.push(square)
-			i++
 		}
 	}
 	createBoard()
