8ecdeadb255f985b9d5c29365cd6c11d52f21597
manishmi <manishmi@MANISHMI-IN.in.oracle.com>
2020-07-28 20:40:36 +0530

    Get the total number of bombs around each valid square

--- app.js
+++ app.js
@@ -20,6 +20,35 @@ document.addEventListener('DOMContentLoaded', () => {
 			grid.appendChild(square)
 			squares.push(square)
 		}
+
+		// add numbers to boxes
+		for (let i = 0; i < squares.length; i++) {
+			let total = 0
+			const isLeftEdge = (i % width === 0)
+			const isRightEdge = (i % width === width - 1)
+
+			if (squares[i].classList.contains('valid')) {
+				// check for bomb in the left box
+				if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
+				// check for bomb in the top right box
+				if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++
+				// check for bomb in the top box
+				if (i > 10 && squares[i - width].classList.contains('bomb')) total++
+				// check for bomb in the top left box
+				if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++
+				// check for bomb in the right box
+				if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+				// check for bomb in the bottom left box
+				if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++
+				// check for bomb in the bottom box
+				if (i < 89 && squares[i + width].classList.contains('bomb')) total++
+				// check for bomb in the bottom right box
+				if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++
+				squares[i].setAttribute('data', total)
+
+				console.log(squares[i])
+			}
+		}
 	}
 	createBoard()
 })
